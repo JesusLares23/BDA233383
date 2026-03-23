3d3a97192397656ad0180ea92d77fb959a69fec9
JesusLares23 <jesus.lares233383@potros.itson.edu.mx>
2026-03-23 15:14:01 -0700

    Primera versión de Empleado Entity acabada

--- (BDA09)EmpresaJPA/(BDAA09)EmpresaJPA/src/main/java/models/Empleado.java
+++ (BDA09)EmpresaJPA/(BDAA09)EmpresaJPA/src/main/java/models/Empleado.java
@@ -3,12 +3,15 @@ package models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import java.io.Serializable;
+import java.time.LocalDate;
 
 /**
  *
@@ -35,14 +38,66 @@ public class Empleado implements Serializable {
             columnDefinition = "DECIMAL CHECK = (salario >= 0)")
     private double salario;
     
+    @Enumerated(EnumType.STRING)
+    private Estatus estatus;
+    
+    @Column(name = "fecha_contratacion", nullable = false)
+    private LocalDate fechaContratacion;
+
+    public Empleado() {
+    }
+    public Empleado(Long id, String nombre, String email, double salario, 
+            Estatus estatus, LocalDate fechaContratacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.salario = salario;
+        this.estatus = estatus;
+        this.fechaContratacion = fechaContratacion;
+    }
+        
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public Estatus getEstatus() {
+        return estatus;
+    }
+    public void setEstatus(Estatus estatus) {
+        this.estatus = estatus;
+    }
+
+    public LocalDate getFechaContratacion() {
+        return fechaContratacion;
+    }
+    public void setFechaContratacion(LocalDate fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+    
     @Override
     public int hashCode() {
         int hash = 0;
--- (BDA09)EmpresaJPA/(BDAA09)EmpresaJPA/src/main/java/models/Estatus.java
+++ (BDA09)EmpresaJPA/(BDAA09)EmpresaJPA/src/main/java/models/Estatus.java
@@ -6,5 +6,5 @@ package models;
  * @author Jesús Pedro Lares Valencia - 00000233383
  */
 public enum Estatus {
-    
+    ACTIVO, INACTIVO;
 }
